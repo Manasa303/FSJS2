9d3d28f575642f9b86ef0600947e5f322d98d3ab
ManasaV <mansa303@gmail.com>
2023-01-24 16:51:35 +0530

    JavaScript Assignment 1 and 2

---
+++ JS_Assignments/Question 01/variables.js
@@ -0,0 +1,14 @@
+// String 
+const string = "I am a String Variable";
+console.log(string);
+
+// Boolean 
+let a = true;
+
+// Undefined
+ const name = undefined;
+
+// Null
+let number = null;
+
+
---
+++ JS_Assignments/Question 02/name.js
@@ -0,0 +1,7 @@
+const fName = "Manasa"
+const lName = "Venkat"
+let mStatus = "Married"
+const country = "India"
+let age = "34"
+
+console.log("Below are my details:" , fName, lName,mStatus, country, age);
