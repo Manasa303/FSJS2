cb0c2f16f97b26893c9ff6b542a096f48c691bac
ManasaV <mansa303@gmail.com>
2023-02-04 11:48:13 +0530

    Adding Question 4 solution

---
+++ JS_Assignments/Question_04/includes.js
@@ -0,0 +1,8 @@
+// Assigning String to a variable
+let text = "Java Script";
+
+// Checking whether word "Script" 
+let check = text.includes("Script");
+
+// Console log to check output
+console.log(check);
