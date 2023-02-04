7380eeb460bdfb5629312c5bf2427708701bb8b1
ManasaV <mansa303@gmail.com>
2023-02-04 11:48:55 +0530

    Adding Question 5 solution

---
+++ JS_Assignments/Question_05/split.js
@@ -0,0 +1,8 @@
+// Assigning String to a variable
+const intro = "My Name is Manasa";
+
+// Checking whether word "Script" 
+let arr1 = intro.split(" ");
+
+// Console log to check output
+console.log(arr1);
