4d7e8756cdcc8d9728d447ce8f74cdbd4901e094
ManasaV <mansa303@gmail.com>
2023-02-04 11:54:07 +0530

    Adding Question 3 solution

--- JS_Assignments/Question 02/name.js
+++
@@ -1,7 +0,0 @@
-const fName = "Manasa"
-const lName = "Venkat"
-let mStatus = "Married"
-const country = "India"
-let age = "34"
-
-console.log("Below are my details:" , fName, lName,mStatus, country, age);
---
+++ JS_Assignments/Question_03/uppercase.js
@@ -0,0 +1,5 @@
+// Assigning a string to a variable
+
+const school = "vidyaniketan";
+
+console.log(school.toUpperCase());
